47f8ce36090d0e1f57bdabe4af241452a6493d33
Joshua Gonzales <gonzales.254036@lucena.sti.edu.ph>
2024-11-18 11:44:45 +0800

    feat: Add UserButton

--- app/(root)/components/RootNavLinks.tsx
+++ app/(root)/components/RootNavLinks.tsx
@@ -7,39 +7,72 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { NAV_LINKS } from "@/lib/constants";
 import RootMobileNavLinks from "./RootMobileNavLinks";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { MessageCircle } from "lucide-react";
 
 const RootNavLinks = () => {
 	const pathname = usePathname();
 
 	return (
 		<>
-			<ul className="hidden md:flex items-center gap-6">
-				{NAV_LINKS.map((link) => {
-					const { href, icon: LinkIcon, label } = link;
+			<div className="flex items-center gap-4">
+				<ul className="hidden md:flex items-center gap-6">
+					{NAV_LINKS.map((link) => {
+						const { href, icon: LinkIcon, label } = link;
 
-					return (
-						<li key={href}>
-							<Button
-								variant={"link"}
-								className={cn(
-									"text-muted-foreground hover:text-primary",
-									pathname === href
-										? "text-primary underline underline-offset-4"
-										: null
-								)}
-								asChild
-							>
-								<Link href={href}>
-									<LinkIcon className="w-4" />
-									{label}
-									{href === "/bag" && <>&nbsp;(0)</>}
-								</Link>
-							</Button>
-						</li>
-					);
-				})}
-			</ul>
-			<RootMobileNavLinks />
+						return (
+							<li key={href}>
+								<Button
+									variant={"link"}
+									className={cn(
+										"text-muted-foreground hover:text-primary",
+										pathname === href
+											? "text-primary underline underline-offset-4"
+											: null
+									)}
+									asChild
+								>
+									<Link href={href}>
+										<LinkIcon className="w-4" />
+										{label}
+										{href === "/bag" && <>&nbsp;(0)</>}
+									</Link>
+								</Button>
+							</li>
+						);
+					})}
+				</ul>
+				<div className="hidden md:block">
+					<SignedIn>
+						{/* Mount the UserButton component */}
+						<UserButton>
+							<UserButton.MenuItems>
+								<UserButton.Action
+									label="Open chat"
+									labelIcon={<MessageCircle />}
+									onClick={() => alert("init chat")}
+								/>
+							</UserButton.MenuItems>
+						</UserButton>
+					</SignedIn>
+				</div>
+			</div>
+
+			<div className="flex md:hidden gap-4">
+			<SignedIn>
+						{/* Mount the UserButton component */}
+						<UserButton>
+							<UserButton.MenuItems>
+								<UserButton.Action
+									label="Open chat"
+									labelIcon={<MessageCircle />}
+									onClick={() => alert("init chat")}
+								/>
+							</UserButton.MenuItems>
+						</UserButton>
+					</SignedIn>
+				<RootMobileNavLinks />
+			</div>
 		</>
 	);
 };
