da45d23eebdce2ae74e248e961e497b8effe6d30
Joshua Gonzales <gonzales.254036@lucena.sti.edu.ph>
2024-11-18 13:52:50 +0800

    style: Hero Section

--- app/(root)/components/Hero.tsx
+++ app/(root)/components/Hero.tsx
@@ -8,18 +8,26 @@ const Hero = () => {
 		<div className="container py-32 lg:py-60 flex items-center justify-center">
 			<div className="flex flex-col items-center gap-6">
 				<div className="flex flex-col gap-4 items-center justify-center">
-					<h1 className="text-hero">SoulePsycle.com</h1>
-					<p className="text-lg text-muted-foreground">
+					<h1 className="break-words text-center text-4xl sm:text-5xl md:text-6xl font-extrabold">
+						SoulePsycle.com
+					</h1>
+
+					<p className="break-words max-w-xl text-center text-lg sm:text-xl md:text-2xl text-muted-foreground">
 						[soul•cycle] shouting your sighs. All our designs are
 						copyright of our store. DO NOT REPRINT.
 					</p>
 				</div>
 
-				<Button className="w-fit text-lg" size={'lg'} asChild>
-                    <Link href={'/shop'}>
-                        Shop Now!<ChevronRightIcon className="w-6" />
-                    </Link>
-                </Button>
+				<Button
+					className="w-fit text-lg md:text-2xl py-6 md:py-8"
+					size={"lg"}
+					asChild
+				>
+					<Link href={"/shop"}>
+						Shop Now!
+						<ChevronRightIcon size={20} />
+					</Link>
+				</Button>
 			</div>
 		</div>
 	);
